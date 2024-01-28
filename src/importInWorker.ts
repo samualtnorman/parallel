671fe984d28b7ccc5d8a3cad1e86bc6fa68ecb1b
Samual Norman <me@samual.uk>
2024-01-28 23:39:03 +0000

    thread to thread communication

--- src/importInWorker.ts
+++ src/importInWorker.ts
@@ -1,9 +1,8 @@
 import type { AnyFunction, Async, Entries, Rejecter, Resolver } from "@samual/lib"
 import { cpus } from "os"
-import { Worker, isMainThread, parentPort } from "worker_threads"
+import { Worker, isMainThread, parentPort, threadId, workerData } from "worker_threads"
 import { MessageTag, type Message } from "./internal"
 
-const WorkerModuleURL = isMainThread ? new URL(`importInWorker.js`, import.meta.url) : undefined
 const idsToPromiseCallbacks = new Map<number, { resolve: Resolver<any>, reject: Rejecter }>
 let idCounter = 0
 
@@ -31,40 +30,73 @@ parentPort?.on(`message`, async (message: Message) => {
 	}
 })
 
-const taskers = isMainThread ? cpus().map(() => {
-	const tasker = { worker: new Worker(WorkerModuleURL!), tasks: 0 }
+let taskCounts: Uint8Array
+let workers: Worker[] | undefined
 
-	tasker.worker.on(`message`, (message: Message) => {
-		if (message.tag == MessageTag.Task) {
-			const otherTasker = taskers!.reduce((previous, current) => previous.tasks > current.tasks ? current : previous)
-			const id = idCounter++
+if (isMainThread) {
+	const cpuInfos = cpus()
+	taskCounts = new Uint8Array(new SharedArrayBuffer(cpuInfos.length))
+	const messagePorts = cpuInfos.map(() => cpuInfos.map((): MessagePort | undefined => undefined))
 
-			otherTasker.tasks++
-			otherTasker.worker.postMessage({ ...message, id } satisfies Message)
+	for (let fromIndex = 4; fromIndex--;) {
+		for (let toIndex = fromIndex; toIndex--;) {
+			const { port1, port2 } = new MessageChannel
 
-			idsToPromiseCallbacks.set(id, {
-				resolve(value) {
-					tasker.worker.postMessage({ tag: MessageTag.Return, id: message.id, value } satisfies Message)
-				},
-				reject(value: any) {
-					tasker.worker.postMessage({ tag: MessageTag.Throw, id: message.id, value } satisfies Message)
-				}
-			})
-		} else {
-			const { resolve, reject } = idsToPromiseCallbacks.get(message.id)!
-
-			idsToPromiseCallbacks.delete(message.id)
-			tasker.tasks--
-
-			if (message.tag == MessageTag.Return)
-				resolve(message.value)
-			else
-				reject(message.value)
+			messagePorts[fromIndex]![toIndex] = port1
+			messagePorts[toIndex]![fromIndex] = port2
 		}
-	})
+	}
 
-	return tasker
-}) : undefined
+	const thisModuleUrl = new URL(import.meta.url)
+
+	workers = cpuInfos.map((_, index) => {
+		const worker = new Worker(thisModuleUrl, {
+			workerData: { ports: messagePorts[index], taskCounts },
+			transferList: messagePorts[index]!.filter(Boolean) as any
+		})
+
+		tasker.worker.on(`message`, (message: Message) => {
+			if (message.tag == MessageTag.Task) {
+				// let otherWorker = workers![0]!
+				// let otherWorkerTaskCount = taskCounts[0]!
+
+				// for (let index = 1; index < )
+
+				const otherTasker = workers!
+					.reduce((previous, current, index) => previous.tasks > current.tasks ? current : previous)
+
+				const id = idCounter++
+
+				otherTasker.tasks++
+				otherTasker.worker.postMessage({ ...message, id } satisfies Message)
+
+				idsToPromiseCallbacks.set(id, {
+					resolve(value) {
+						tasker.worker.postMessage({ tag: MessageTag.Return, id: message.id, value } satisfies Message)
+					},
+					reject(value: any) {
+						tasker.worker.postMessage({ tag: MessageTag.Throw, id: message.id, value } satisfies Message)
+					}
+				})
+			} else {
+				const { resolve, reject } = idsToPromiseCallbacks.get(message.id)!
+
+				idsToPromiseCallbacks.delete(message.id)
+				tasker.tasks--
+
+				if (message.tag == MessageTag.Return)
+					resolve(message.value)
+				else
+					reject(message.value)
+			}
+		})
+
+		return tasker
+	})
+} else {
+	taskCounts = workerData.taskCounts
+	console.log(threadId, workerData)
+}
 
 /** @example
   * const heavyFunction = importInWorker<typeof import("./heavyFunction.js"), "heavyFunction">(
