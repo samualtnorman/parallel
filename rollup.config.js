bfc7b55987202c5774119d8aa38553bec2a8b0c2
Samual <me@samual.uk>
2024-09-24 14:02:57 +0100

    switch to `@samual/rollup-config`

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,4 @@
 #!node_modules/.bin/rollup --config
-import babelPresetEnv from "@babel/preset-env"
-import babelPresetTypescript from "@babel/preset-typescript"
-import { babel } from "@rollup/plugin-babel"
-import { nodeResolve } from "@rollup/plugin-node-resolve"
-import terser from "@rollup/plugin-terser"
-import { findFiles } from "@samual/lib/findFiles"
-import { cpus } from "os"
+import { rollupConfig } from "@samual/rollup-config"
 
-/** @typedef {import("rollup").RollupOptions} RollupOptions */
-/** @typedef {import("@babel/preset-env").Options} BabelPresetEnvOptions */
-
-const SOURCE_FOLDER = "src"
-
-/** @type {RollupOptions} */ export default {
-	input: Object.fromEntries(
-		(await findFiles(SOURCE_FOLDER)).filter(path => path.endsWith(".ts") && !path.endsWith(".d.ts"))
-			.map(path => [ path.slice(SOURCE_FOLDER.length + 1, -3), path ])
-	),
-	output: { dir: "dist" },
-	plugins: [
-		nodeResolve({ extensions: [ ".ts" ] }),
-		babel({
-			babelHelpers: "bundled",
-			extensions: [ ".ts" ],
-			presets: [
-				[ babelPresetEnv, /** @satisfies {BabelPresetEnvOptions} */ ({ targets: { node: "18.19" } }) ],
-				babelPresetTypescript
-			]
-		}),
-		terser({ keep_fnames: true, compress: { passes: Infinity }, maxWorkers: Math.floor(cpus().length / 2) })
-	],
-	strictDeprecations: true,
-	treeshake: { moduleSideEffects: false }
-}
+export default rollupConfig()
